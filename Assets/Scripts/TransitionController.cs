226ac1d6d8cfb7d90b15e98b9c3a212f108e870c
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2022-10-27 15:07:41 +0700

    Initial Commit

---
+++ Assets/Scripts/TransitionController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransitionController
+{
+
+    public static IEnumerator ActivateTransition(Material transitionMaterial, string propertyName, float duration, bool isEnterMode = true, Action todo = null)
+    {
+        if (isEnterMode)
+        {
+            transitionMaterial.SetFloat(propertyName, 0f);
+
+            while (transitionMaterial.GetFloat(propertyName) <= 1f)
+            {
+                transitionMaterial.SetFloat(propertyName, Mathf.MoveTowards(transitionMaterial.GetFloat(propertyName), 1f, 1 / duration * Time.deltaTime));
+                yield return null;
+            }
+        }
+        else
+        {
+            transitionMaterial.SetFloat(propertyName, 1f);
+
+            while (transitionMaterial.GetFloat(propertyName) >= 0f)
+            {
+                transitionMaterial.SetFloat(propertyName, Mathf.MoveTowards(transitionMaterial.GetFloat(propertyName), 0f, 1 / duration * Time.deltaTime));
+                yield return null;
+            }
+        }
+
+        if (todo != null)
+        {
+            todo();
+        }
+    }
+
+    // Call for using Transition via animator
+    //public static IEnumerator ActivateTransition(Animator transitionAnimator, string parameterToTrigger, float duration)
+    public static IEnumerator ActivateTransition(Animator transitionAnimator, string parameterToTrigger, float duration)
+    {
+        transitionAnimator.SetTrigger(parameterToTrigger);
+        string clipName = transitionAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+        //Debug.Log(clipName + ", " + transitionAnimator.GetCurrentAnimatorStateInfo(0).IsName(clipName));
+
+        while (transitionAnimator.GetCurrentAnimatorStateInfo(0).IsName(clipName))
+        {
+            Debug.Log(clipName);
+            yield return null;
+        }
+    }
+
+    public static IEnumerator ActivateTransition(Animator transitionAnimator, string parameterToTrigger, float duration = 0, Action todo = null)
+    {
+        transitionAnimator.SetTrigger(parameterToTrigger);
+        string clipName = transitionAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+        while (transitionAnimator.GetCurrentAnimatorStateInfo(0).IsName(clipName))
+        {
+            Debug.Log(clipName);
+            yield return null;
+        }
+
+        if (todo != null)
+        {
+            todo();
+        }
+    }
+}
