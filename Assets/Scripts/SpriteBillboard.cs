dc562b6836c3feee5afa87d994af521aa8027312
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2022-10-29 22:12:29 +0700

    Update SpriteBillboard

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -1,13 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
-    [SerializeField] private bool freezeXZAxis = true;
+    private Transform _mainTransform;
+    private Animator _animator;
+    private SpriteRenderer _renderer;
 
-    void Update()
+    [SerializeField] private bool freezeXZAxis = true;
+    [SerializeField] private float backAngle = 65f;
+    [SerializeField] private float sideAngle = 155f;
+
+    private void Start()
     {
+        _mainTransform = GetComponentInParent<Transform>();
+        _animator = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        Vector3 camForwardVector = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z);
+
         if (freezeXZAxis)
         {
             transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
@@ -16,5 +29,36 @@ public class SpriteBillboard : MonoBehaviour
         {
             transform.rotation = Camera.main.transform.rotation;
         }
+
+        float signedAngle = Vector3.SignedAngle(_mainTransform.forward, camForwardVector, Vector3.up);
+
+        Vector2 animationDirection = new Vector2(0f, -1f);
+
+        float angle = Mathf.Abs(signedAngle);
+
+        if (angle < backAngle) // Look from Back
+        {
+            animationDirection = new Vector2(0f, -1f);
+        }
+        else if (angle < sideAngle) // Look from Side
+        {
+            animationDirection = new Vector2(1f, 0f);
+
+            if (signedAngle < 0)
+            {
+                _renderer.flipX = true;
+            }
+            else
+            {
+                _renderer.flipX = false;
+            }
+        }
+        else // Look from Neither (Front)
+        {
+            animationDirection = new Vector2(0f, 1f);
+        }
+
+        _animator.SetFloat("X", animationDirection.x);
+        _animator.SetFloat("Y", animationDirection.y);
     }
 }
