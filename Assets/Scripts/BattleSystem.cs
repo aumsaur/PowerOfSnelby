cc536b6002ef962ec338047f3818f98820332766
AUMSTER <74236586+aumsaur@users.noreply.github.com>
2022-11-28 18:24:11 +0700

    Update Turnbase system and scene

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -16,16 +16,23 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] private Transform playerStation;
     [SerializeField] private Transform enemyStation;
 
+    [SerializeField] private TMP_Text basePlayerText;
+    [SerializeField] private TMP_Text powerPlayerText;
+    [SerializeField] private TMP_Text[] baseEnemyText;
+    [SerializeField] private TMP_Text[] powerEnemyText;
+
+    [SerializeField] private GameObject floatingTextPrefabs;
+    private GameObject newFloatingText;
+
+    [VectorLabels("Base Input", "Power Input"), SerializeField] private Vector2 input = new Vector2(1,1);
+
+    [SerializeField] private float comboTime;
+
     [SerializeField] private Text dialogueText;
 
-    [SerializeField] private TMP_Text baseInputText;
-    [SerializeField] private TMP_Text powerInputText;
+    private int multiplier = 1;
+    [SerializeField] private TMP_Text multiplierText;
 
-    [SerializeField] private float baseInput = 1;
-    [SerializeField] private float powerInput = 1;
-
-    private int solvedAnswer;
-    
     private CharacterAttribute playerAttribute, enemyAttribute;
 
     private Solve solveEq;
@@ -34,6 +41,15 @@ public class BattleSystem : MonoBehaviour
     void Start()
     {
         state = BattleState.START;
+        basePlayerText.text = input.x.ToString();
+        powerPlayerText.text = input.y.ToString();
+
+        StartCoroutine(SetupBattle());
+    }
+
+    private void Update()
+    {
+        
     }
 
     private IEnumerator SetupBattle()
@@ -47,67 +63,92 @@ public class BattleSystem : MonoBehaviour
         yield return new WaitForSeconds(1);
 
         state = BattleState.GENERATE;
+        UpdateProblem();
     }
 
     private void EndBattle()
     {
-
+        dialogueText.text = state == BattleState.WON ? "Win" : state == BattleState.LOST ? "Defeat" : dialogueText.text;
     }
 
     private void UpdateProblem()
     {
         solveEq = ProblemGenerator.GenerateEquation();
 
-        
+        Debug.Log(solveEq.problem + ": " + solveEq.answer);
     }
 
-    private void UpdateAnswer()
-    {
-
-    }
-
-    private bool CheckAnswer(int input)
+    private bool CheckAnswer(float input)
     {
         // in case of user correct or not
-
-        if (input == solveEq.answer)
-        {
-            // user correct then ...
-            return true;
-        }
-        else
-        {
-            // user not correct then ...
-            return false;
-        }
+        return input == solveEq.answer;
     }
 
     public void OnSubmitButton()
     {
         // validation user input
+        if (!CheckAnswer(Mathf.Pow(input.x, input.y)))
+        {
+            if (playerAttribute.healthValue > 0)
+            {
+                playerAttribute.DecreaseHealth(1);
+            }
+            multiplier = 1;
+            state = BattleState.WRONG;
+        }
+        else
+        {
+            // if combo time frame dont run out ; ++multiplier
+            if (enemyAttribute.healthValue > 0)
+            {
+                enemyAttribute.DecreaseHealth(multiplier);
+            }
+            multiplier += 1;
 
-        // monster turn
+            state = BattleState.RIGHT;
+        }
 
         // generate new problem
+        UpdateProblem();
     }
 
     public void OnRaisePowerButton()
     {
-
+        if (input.y < 24)
+        {
+            input.y += 1;
+        }
+        else
+            input.y = 0;
     }
 
     public void OnRaiseBaseButton()
     {
-
+        if (input.x < 24)
+        {
+            input.x += 1;
+        }
+        else
+            input.x = 0;
     }
 
-    public void OnRedycePowerButton()
+    public void OnReducePowerButton()
     {
-
+        if (input.y < 1)
+        {
+            input.y = 25;
+        }
+        else
+            input.y -= 1;
     }
 
     public void OnReduceBaseButton()
     {
-
+        if (input.x < 1)
+        {
+            input.x = 25;
+        }
+        else
+            input.x -= 1;
     }
 }
